4ec575336ceb2447866f782a10cb7d9986bd2f1e
Fidel Isaboke <fidelisaboke@gmail.com>
2025-02-15 20:45:02 +0300

    feat: add theme switching functionality

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const btnThemeSwitch = document.getElementById("theme-switch");
+const themeSwitcherDiv = document.querySelector(".theme-switcher");
+const themeSwitcherIcon = themeSwitcherDiv.querySelector("i");
+const body = document.body;
+
+function loadLocalStorageData() {
+    const themeSwitcherIconClass = themeSwitcherIcon.classList[1];
+    const theme = localStorage.getItem("theme");
+    const isLightMode = theme === "light-theme" || !theme;
+    body.classList.toggle("light-theme", isLightMode);
+    themeSwitcherIcon.classList.replace(
+        themeSwitcherIconClass,
+        isLightMode ? "bx-moon" : "bx-sun"
+    );
+}
+
+loadLocalStorageData();
+
+btnThemeSwitch.addEventListener("click", () => {
+    const themeSwitcherIconClass = themeSwitcherIcon.classList[1];
+    localStorage.setItem(
+        "theme",
+        body.classList.contains("light-theme") ? "dark-theme" : "light-theme"
+    );
+    const isLightMode = body.classList.toggle("light-theme");
+    const classReplaced = themeSwitcherIcon.classList.replace(
+        themeSwitcherIconClass,
+        isLightMode ? "bx-moon" : "bx-sun"
+    );
+    console.log(classReplaced);
+    console.log(themeSwitcherIcon.classList);
+});
